546fe4bb96cc6fb9c5998ca87d7e95f0f001bb24
Dull-keen <dulkin2002@gmail.com>
2025-11-19 13:41:12 +0300

    Объяснил наблюдаемые выводы

--- lab2/task2.c
+++ lab2/task2.c
@@ -11,6 +11,7 @@ int main()
 
     if (choice == 1)
     {
+        printf("\nПечатается PID дочернего процесса (отец) или 0 (сын) в случайном порядке:\n\n");
         int pid;
         pid=fork();
         printf("%d\n", pid);
@@ -18,6 +19,7 @@ int main()
     }
     else if (choice == 2)
     {
+        printf("\nПечатается 'Hi' 6 раз в случайном порядке (2 раза отец, 2 раза сын, 1 раз другой сын, 1 раз внук):\n\n");
         fork();
         printf("Hi\n");
         fork();
@@ -26,12 +28,14 @@ int main()
     }
     else if (choice == 3)
     {
+        printf("\nПечатается 'forked!' и (PID/0) 10 раз в случайном порядке, причем ровно 5 раз будет 0 (на листьях дерева процессов):\n\n");
         if (fork() || fork()) fork();
-        printf("forked! %d", fork());
+        printf("forked! %d ", fork());
         return 0;
     }
     else if (choice == 4)
     {
+        printf("\nПечатается число 52 ровно 7 раз в случайном порядке (1 отец, 2 сына, 2 внука, 2 правнука):\n\n");
         if (fork() && (!fork()))
         {
         if (fork() || fork()) fork();
